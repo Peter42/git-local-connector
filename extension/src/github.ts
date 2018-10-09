ade8610877be705ddc0e0ff8c0c67a4aad566487
Niels Becker <aaqqws@googlemail.com>
2018-10-09 11:39:26 +0200

    fix tslint

--- extension/src/github.ts
+++ extension/src/github.ts
@@ -1,13 +1,19 @@
+//tslint:disable:max-line-length
 import { GitLocalService } from "./GitLocalService";
 import { LocalRepo } from "./interfaces";
 import { RpcClient } from "./RpcClient";
 
+/**
+ * Github entry file
+ */
+
 const client = new RpcClient();
-client.connect().then(async () => {
-    let currentRepo: LocalRepo | null = null;
+const api = new GitLocalService(client);
+let currentRepo: LocalRepo | null = null;
 
-    const api = new GitLocalService(client);
+client.connect().then(init).catch(() => { /* ignore */ });
 
+async function init() {
     api.findRepo(window.location.pathname.split("/")[2]).then(repo => {
         currentRepo = repo;
         if (!repo) {
@@ -31,73 +37,88 @@ client.connect().then(async () => {
                 }
             ]);
         }
-    })
+    });
+}
 
+function showCloneButton() {
+    // TODO: get from options
+    const cloneLocation = "";
+    let cloneButton: HTMLElement | null;
+    cloneButton = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.mt-2 > a.btn.btn-outline.get-repo-btn.tooltipped.tooltipped-s.tooltipped-multiline.js-get-repo");
+    if (cloneButton == null) {
+        console.error("cloneButton is null");
 
-    function showCloneButton() {
-        let cloneLocation = "";
-        let cloneButton: HTMLElement | null;
-        cloneButton = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.mt-2 > a.btn.btn-outline.get-repo-btn.tooltipped.tooltipped-s.tooltipped-multiline.js-get-repo");
-        if (cloneButton == null) {
-            console.error("cloneButton is null");
+        return;
+    }
+    cloneButton.innerText = "Clone";
+    cloneButton.removeAttribute("href");
+    cloneButton.addEventListener("click", event => {
+        event.cancelBubble = true;
+        let cloneUrlInput: HTMLInputElement | null;
+        cloneUrlInput = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.clone-options.https-clone-options > div > input");
+        if (cloneUrlInput == null) {
+            console.error("cloneUrlInput is null");
+
             return;
         }
-        cloneButton.innerText = "Clone";
-        cloneButton.removeAttribute("href");
-        cloneButton.addEventListener("click", event => {
-            event.cancelBubble = true;
-            let cloneUrlInput: HTMLInputElement | null;
-            cloneUrlInput = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.clone-options.https-clone-options > div > input");
-            if (cloneUrlInput == null) {
-                console.error("cloneUrlInput is null");
-                return;
-            }
-            let cloneUrl = cloneUrlInput.value;
-            api.clone(cloneUrl, cloneLocation).then(showCloneOutput);
-        });
+        const cloneUrl = cloneUrlInput.value;
+        api.clone(cloneUrl, cloneLocation).then(showCloneOutput);
+    });
+}
+
+function showCloneOutput(cloneoutput: string) {
+    const parent = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.mt-2");
+    const div = document.createElement("div");
+    div.innerText = cloneoutput;
+    if (parent == null) {
+        console.error("parent is null");
+
+        return;
     }
+    parent.appendChild(div);
+}
 
-    function showCloneOutput(cloneoutput: string) {
-        const parent = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-navigation.in-mid-page.d-flex.flex-items-start > details > div > div > div.get-repo-modal-options > div.mt-2");
-        const div = document.createElement("div");
-        div.innerText = cloneoutput;
-        if (parent == null) {
-            console.error("parent is null");
-            return;
-        }
-        parent.appendChild(div);
+function openFolder() {
+    if (!currentRepo) {
+        throw new Error("Repo is not cloned");
     }
+    api.open(currentRepo);
+}
 
+interface SetBarAction {
+    handler?: EventListenerOrEventListenerObject;
+    href?: string;
+    icon: string;
+    text: string;
+}
+function setBar(text: string, actions: SetBarAction[]) {
 
-    function openFolder() {
-        if (!currentRepo) throw new Error("Repo is not cloned");
-        api.open(currentRepo);
-    }
-
-    function setBar(text: string, actions: Array<any>) {
-
-        function actionToNode(action: any) {
-            let a = document.createElement("a");
-            a.className = "muted-link";
-            a.innerHTML = `${action.icon} ${action.text}`;
+    function actionToNode(action: SetBarAction) {
+        const a = document.createElement("a");
+        a.className = "muted-link";
+        //tslint:disable-next-line:no-inner-html
+        a.innerHTML = `${action.icon} ${action.text}`;
+        if (action.handler) {
             a.addEventListener("click", action.handler);
-            return a;
         }
 
-        let span = document.createElement("span");
-        span.className = "float-right";
-        actions.map(actionToNode).forEach(span.appendChild.bind(span));
-
-        let div = document.createElement("div");
-        div.className = "branch-infobar";
-        div.innerText = text;
-        div.appendChild(span);
-
-        let sibling = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.commit-tease.js-details-container.Details.d-flex");
-        if (sibling == null || sibling.parentElement == null) {
-            console.error("problem finding sibling");
-            return;
-        }
-        sibling.parentElement.insertBefore(div, sibling);
+        return a;
     }
-}).catch(()=>{});
+
+    const span = document.createElement("span");
+    span.className = "float-right";
+    actions.map(actionToNode).forEach(span.appendChild.bind(span));
+
+    const div = document.createElement("div");
+    div.className = "branch-infobar";
+    div.innerText = text;
+    div.appendChild(span);
+
+    const sibling = document.querySelector("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.commit-tease.js-details-container.Details.d-flex");
+    if (sibling == null || sibling.parentElement == null) {
+        console.error("problem finding sibling");
+
+        return;
+    }
+    sibling.parentElement.insertBefore(div, sibling);
+}
